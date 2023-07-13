c5fb02823e5eeb0ab94b1fbb57f6548482b5a544
Jim Junior <jimjunior854@gmail.com>
2023-07-13 17:19:40 +0300

    add sj

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className={styles.content}>
         <h1 className={styles.title} id="about" >About the <span className="">Startup</span> Act</h1>
         <div className={styles.description}>
-          <p>The Uganda Startup Act project is a joint initiative by Ugandas tech startup ecosystem and the Presidency to harness the potential of our digital economy through co-created regulations. The Act is aimed at ensuring that Ugandas laws and regulations are clear, planned and work for the tech ecosystem. This, we believe, will contribute to the creation of an enabling environment for the growth of the ecosystem, as well as the attraction and protection of investment in tech startups.</p>
+          <p>The Uganda Startup Act project is a joint initiative by Ugandas tech startup ecosystem and the Ministry of Trade (MTIC) to harness the potential of our digital economy through co-created regulations. The Act is aimed at ensuring that Ugandas laws and regulations are clear, planned and work for the tech ecosystem. This, we believe, will contribute to the creation of an enabling environment for the growth of the ecosystem, as well as the attraction and protection of investment in tech startups.</p>
         </div>
       </div>
     </section>
