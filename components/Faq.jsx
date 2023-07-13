80fe1373d02e2e1533a4a161877369046a02df00
Jim Junior <jimjunior854@gmail.com>
2023-07-13 17:14:51 +0300

    Add partners

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -10,18 +10,14 @@ const FAQ = () => {
           <div className={styles.flex}>
             <div className={styles.column}>
               <article className={styles.article}>
-                <h3>Who is leading this initiative?</h3>
-                <p>The Uganda Startup Act project is a joint initiative by Uganda’s tech ecosystem and the Presidency to harness the potential of our digital economy through co-created regulations. While this is our act; it is being project managed by the Ventures Platform Foundation and supervised by the Presidential Strategic Advisory Group composed of stakeholder representatives from the tech-startup ecosystem in Uganda.</p>
+                <h3>Here is the subject matter context?</h3>
+                <p>Under the Ministry of Trade, Industry and Cooperatives, the Government of Uganda is exploring the potential of startups and innovations to spur economic growth and development in the country.</p>
+                <p>From a High Level Policy Dialogue on the “Relevance of a Startup Policy in Uganda” hosted by Private Sector Foundation Uganda (PSFU) on May 30th, 2021 at Sheraton Kampala Hotel; the Ministry of Trade Industry and Co-operatives (MTIC) was requested to develop a National Startup Policy by the host on behalf of other stakeholders in the ecosystem. As a result, the MTIC constituted a multisectoral Technical Working Group (TWG) to spearhead the development of interventions geared towards addressing the challenges which inhibit the establishment, sustained performance and growth of startups in Uganda.  The TWG comprises of technical members of MTIC, PSFU, Startup Uganda and The Innovation Village. These entities will carry on activities as guided for this policy reform process. The Nominations were formalized on May 10th, 2022 by the MTIC PS. Again, “The minimum expectations are a law, a policy, a strategy or a code for Startups.”</p>
               </article>
 
               <article className={styles.article}>
-                <h3>How do we ensure all tech sectors are covered?</h3>
-                <p>By jointly reviewing the drafts of the act and giving your inputs. You can participate by clicking on here</p>
-              </article>
-
-              <article className={styles.article}>
-                <h3>What is the scope of this project?</h3>
-                <p>We plan that this act is submitted to the NASS by October and passed by 2021. Visit the project timeline and subscribe to the newsletter to follow the journey.</p>
+                <h3>What Next?</h3>
+                <p>The process is still on going, however, we are in the final stages of the Regulatory Impact Assessment report approval processes from the consultant. We hope that in the next few weeks or months, the report shall be then presented to the cabinet secretariat that would then advise on next steps from the proposals</p>
               </article>
             </div>
             <div className={styles.column}>
@@ -29,10 +25,14 @@ const FAQ = () => {
                 <h3>Has this been successful anywhere else?</h3>
                 <p>Yes. Other African countries like Senegal and Tunisia have passed their own startup bills.</p>
               </article>
+
               <article className={styles.article}>
-                <h3>How is this different from past attempts?</h3>
-                <p>We are adopting a collaborative approach to drive this cause. We have tagged this collaborative framework the Big Tent approach, which in other words, means the bill is owned by the ecosystem. The process of engagement involves all relevant stakeholders in the startup ecosystem including the government (both at regional and federal levels, as well as across the legislative, executive and judiciary arms of government to develop and submit the bill to be signed into law. This is an initiative together with #i4Policy, and, is being supported by the Innovation for Policy Foundation, an organization working to increase public policy co-creation that has experience in facilitating the co-design of startup policy and legislative frameworks in more than a dozen African countries. We are also identifying existing laws/regulations and harmonising them into this bill. We are also using the bill as an opportunity to update existing but outdated laws.</p>
+                <h3>Why take such a long and tedious process?</h3>
+                <p>As opposed to private members bill that is prepared through a parliamentarians internal process and later directly to the floor of parliament, the government process differs very much. The governments framework requires a process that involves a Regulatory Impact Assessment Report development process which provide evidence on the right interventions to address the existing challenges inhibiting growth of the startups in Uganda.</p>
+                <p>And as a way of facilitating public policy dialogue, consulting the parties that are directly affected by the absence of certain interventions is mandatory. The Startup entrepreneurs and innovators, especially young people in different sectors of the economy were consulted about the challenges which inhibit their innovations and the possible actions which Government can take to address these challenges through Regional Consultative meetings.</p>
               </article>
+
+
             </div>
 
           </div>
--- components/Partners.jsx
+++ components/Partners.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styles from "./styles/Partners.module.scss"
 import Image from "next/image"
 import foundersLounge from "@/assets/fl.png"
+import innovationVillage from "@/assets/iv.png"
+import psfu from "@/assets/psfu.png"
+import th from "@/assets/th.png"
+import SU from "@/assets/SU.png"
+import Link from 'next/link'
 
 const Partners = () => {
   return (
@@ -12,15 +17,30 @@ const Partners = () => {
           <p>We work with a set of genrous organisations and individuals that believe in our cause</p>
           <div className={styles.partners}>
 
+
             <div className={styles.partner}>
-              <Image src={foundersLounge} width={200} height={100} alt="founders loung" />
+              <Image src={innovationVillage} width={200} height={100} alt="founders loung" />
             </div>
+
+            <div className={styles.partner}>
+              <Image src={psfu} width={200} height={100} alt="founders loung" />
+            </div>
+
+            <div className={styles.partner}>
+              <Image src={SU} width={200} height={100} alt="founders loung" />
+            </div>
+
+            <div className={styles.partner}>
+              <Image src={th} width={200} height={100} alt="founders loung" />
+            </div>
+
+
 
           </div>
 
         </div>
       </div>
-    </section>
+    </section >
   )
 }
 
